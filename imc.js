34d4d1038761e945f1f75097f1ac2701083dcbcd
Victor Melo <contatomelo2@gmail.com>
2023-01-31 17:58:24 -0300

    update js

---
+++ imc.js
@@ -0,0 +1,26 @@
+const calcular = document.getElementById('calcular');
+
+function imc () {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && altura !== '' && peso !== '') {
+        
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = '';
+
+        if (valorIMC < 18.5){
+            classificacao = 'Abaixo do peso.'
+        }
+
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`
+
+    }else {
+        resultado.textContent = 'Preencha todos os campos!!!';
+    }
+}
+
+calcular.addEventListener('click', imc);
